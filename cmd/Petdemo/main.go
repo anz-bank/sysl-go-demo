a05273972cee19668b3df779e0a931cf12a0e5b6
Jack Zheng <jack.zheng@anz.com>
2020-10-13 14:08:47 +1100

    Wire up handler

--- cmd/Petdemo/main.go
+++ cmd/Petdemo/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-    "context"
-    "log"
+	"context"
+	"log"
 
-    "github.com/anz-bank/sysl-go/core"
+	"github.com/anz-bank/sysl-go/core"
 
-    "github.com/anz-bank/sysl-go-demo/gen/pkg/servers/Petdemo"
+	petdemo "github.com/anz-bank/sysl-go-demo/gen/pkg/servers/Petdemo"
+	"github.com/anz-bank/sysl-go-demo/internal/handlers"
 )
 
 func main() {
-    type AppConfig struct {
-        // Define app-level config fields here.
-    }
-    log.Fatal(petdemo.Serve(context.Background(),
-        func(ctx context.Context, config AppConfig) (*petdemo.ServiceInterface, *core.Hooks, error) {
-            // Perform one-time setup based on config here.
-            return &petdemo.ServiceInterface{
-                // Add handlers here.
-            }, nil, nil
-        },
-    ))
+	type AppConfig struct {
+		// Define app-level config fields here.
+	}
+	log.Fatal(petdemo.Serve(context.Background(),
+		func(ctx context.Context, config AppConfig) (*petdemo.ServiceInterface, *core.Hooks, error) {
+			// Perform one-time setup based on config here.
+			return &petdemo.ServiceInterface{
+				GetPetList: handlers.GetRandomPetPicListRead,
+			}, nil, nil
+		},
+	))
 }
